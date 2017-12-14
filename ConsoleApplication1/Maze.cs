44910d384bd976c6d7617934322a374d7390291b
Simon Thériault <thes2907@usherbrooke.ca>
2017-12-14 13:28:54 -0500

    Added basic constructor

--- ConsoleApplication1/Maze.cs
+++ ConsoleApplication1/Maze.cs
@@ -10,10 +10,10 @@ namespace ConsoleApplication1
     {
         public class Coordinates
         {
-            public uint x;
-            public uint y;
+            public int x;
+            public int y;
 
-            public Coordinates(uint x, uint y)
+            public Coordinates(int x, int y)
             {
                 this.x = x;
                 this.y = y;
@@ -24,16 +24,31 @@ namespace ConsoleApplication1
         public const int NOMINAL = 1;
         public const int FREE = 0;
 
-        int[][] grid;
-
+        int[,] grid;
         Coordinates startPosition;
         Coordinates endPosition;
 
-        public int valueAt(uint x, uint y)
+
+        public Maze(int width, int height)
+        {
+            if (width < 0 || height < 0)
+                throw new Exception("No negative matrix size allowed");
+
+            grid = new int[width, height];
+            startPosition = new Coordinates(0, 0);
+            endPosition = new Coordinates(width, height);
+        }
+
+        public Maze()
+        {
+
+        }
+
+        public int valueAt(int x, int y)
         {
             int value;
 
-            value = grid[x][y];
+            value = grid[x, y];
 
             return value;
         }
@@ -42,14 +57,24 @@ namespace ConsoleApplication1
             return valueAt(position.x, position.y);
         }
 
-        public bool isTraversible(uint x, uint y)
+        public bool isTraversible(int x, int y)
         {
-            return grid[x][y] >= 0;
+            return grid[x, y] >= 0;
         }
 
         public bool isTraversible(Coordinates position)
         {
             return isTraversible(position.x, position.y);
+        }
+
+        public int getHeight()
+        {
+            return grid.GetLength(1);
+        }
+
+        public int getWidth()
+        {
+            return grid.GetLength(0);
         }
     }
 }
